8e55296ca3ffe64e9d9a418db0bd3bb26ea320a6
keith.dodson <keith.dodson@vivint.com>
2019-03-14 15:44:45 -0600

    fixing more of the setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ $(uname) = "Darwin" ]; then
-    brew install imagemagick
+    brew install imagemagick@6
 else:
     sudo apt-get update &&
     sudo apt-get install -y imagemagick libmagickwand-dev
